4de4f70c00f2957554ec1086912966f19a30ff6d
doxka <94704861+karl-lechartier@users.noreply.github.com>
2022-02-01 14:13:08 +0100

    Add files via upload

--- GestionFormulaire/actions/addFormulaire.php
+++ GestionFormulaire/actions/addFormulaire.php
@@ -2,12 +2,14 @@
 
 $titre = filter_input(INPUT_POST, "titre");
 $description = filter_input(INPUT_POST, "description");
-$img = filter_input(INPUT_POST, "img");
-$photo = $_FILES["img"];
+//$img = filter_input(INPUT_POST, "img");
+$image = $_FILES["img"];
 $couleur = filter_input(INPUT_POST, "couleur");
 $date_e = filter_input(INPUT_POST, "date_e");
 $date_f = filter_input(INPUT_POST, "date_f");
 $secteur = filter_input(INPUT_POST, "secteur");
+
+$nomIMG = " ";
 
 include_once "../../config.php";
 $pdo = new PDO("mysql:host=" . Config::SERVEUR . "; dbname=" . Config::BDO, Config::UTILISATEUR, Config::MOTDEPASSE);
@@ -16,7 +18,7 @@ $requete = $pdo->prepare("INSERT INTO `formulaires` (id, titre, description, img
 
 $requete->bindParam(":titre", $titre);
 $requete->bindParam(":description", $description);
-$requete->bindParam(":imgsrc", $img);
+$requete->bindParam(":imgsrc", $nomIMG);
 $requete->bindParam(":couleur", $couleur);
 $requete->bindParam(":date_evenement", $date_e);
 $requete->bindParam(":date_fin", $date_f);
@@ -24,13 +26,19 @@ $requete->execute();
 
 $id = $pdo->lastInsertId();
 
+$nomIMG = $id."-".basename($image["name"]);
+move_uploaded_file($image["tmp_name"], "../../IMG/$nomIMG");
+
 $requete = $pdo->prepare("INSERT INTO `secteursparformulaires` (id_formulaires, id_secteurs) VALUES (:id_formulaires, :id_secteurs)");
 $requete->bindParam(":id_formulaires", $id);
 $requete->bindParam(":id_secteurs", $secteur);
 $requete->execute();
 
-//echo $photo;
-//move_uploaded_file($photo["tmp_name"], "../" . $cheminPhoto);
+$requete = $pdo->prepare("update formulaires set imgsrc=:photo where id=:id");
+$requete->bindParam(":photo", $nomIMG);
+$requete->bindParam(":id", $id);
+$requete->execute();
 
-//header("location: ../adminFormulaire.php");
+echo 'oui';
+header("location: ../adminFormulaire.php");
 ?>
